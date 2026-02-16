32ebda6f9882a79e0f856ca9b22a79329420d798
Mitchell Brown <mitch7553@gmail.com>
2026-02-16 14:17:27 +0800

    fixing the configuration

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const image = require('./controllers/image');
 const db= knex({
         client: 'pg',
         connection: {
+          connetionString:process.env.DATABASE_URL,
           host : process.env.DATABASE_HOST,  
           port: 5432, 
           user: process.env.DATABASE_USER,
